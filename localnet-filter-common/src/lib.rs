02b5bc73e57ac77e4b48768c3c3a1e1e55254994
Jörn Franke <jornfranke@gmail.com>
2022-10-08 17:23:29 +0200

    init

--- localnet-filter-common/src/lib.rs
+++ localnet-filter-common/src/lib.rs
@@ -1,49 +1,53 @@
 #![no_std]
 
-
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct PacketLog {
-    pub ipv4_address: u32,
+    pub ip_address: [u32; 4],
+    pub ip_version: u32,
     pub action: i32,
-    pub uid: u32
+    pub uid: u32,
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct Netfilter {
+    pub filter: [(u128, u128); MAX_ENDPOINT_ENTRIES_USER],
 }
 
 #[cfg(feature = "user")]
 unsafe impl aya::Pod for PacketLog {}
+#[cfg(feature = "user")]
+unsafe impl aya::Pod for Netfilter {}
 
+pub const MAX_ENDPOINT_ENTRIES_USER: usize = 10; // should be always a multiplicator of two as we need to configure a prefix and a range
 
-pub const MAX_ENDPOINT_ENTRIES_USER: usize =10; // should be always a multiplicator of two as we need to configure a prefix and a range
-
-/// Minimal function to determine if an ip is contained in a given range (or CIDR). This works for IPv4 and IPv6. 
+/// Minimal function to determine if an ip is contained in a given range (or CIDR). This works for IPv4 and IPv6.
 /// We work here with primitive types only and do not provide conversion of an IP (range) string to u128 as this function must be also usable in a eBPF module
 ///
 /// # Arguments
-/// * `range_mask` - mask of the range that determines the size of the range
 /// * `prefix` - prefix of the range
+/// * `range_mask` - mask of the range that determines the size of the range
 /// * `ip` - IP to check if is in the given IP address range
-/// 
+///
 /// Returns true if IP is contained in range, false if not
-pub fn range_contains_ip(range_mask: u128, prefix: u128, ip: u128) -> bool {
+pub fn range_contains_ip(prefix: u128, range_mask: u128, ip: u128) -> bool {
     ip & range_mask == prefix
 }
 
-
-
 #[cfg(test)]
 mod tests {
-
 
     #[test]
     fn test_cidrv4_ipv4_local() {
         let range_mask: u128 = 0xff000000; // /8
         let prefix: u128 = 2130706432; // 127.0.0.0 (prefix)
-        // inside
+                                       // inside
         let ip_in: u128 = 2130706433; // 127.0.0.1 (loopback)
-        assert!(super::range_contains_ip(range_mask, prefix, ip_in));
+        assert!(super::range_contains_ip(prefix, range_mask, ip_in));
         // outside
         let ip_out: u128 = 3232281089; // 192.168.178.1
-        assert_eq!(false, super::range_contains_ip(range_mask, prefix, ip_out));
+        assert_eq!(false, super::range_contains_ip(prefix, range_mask, ip_out));
     }
 
     #[test]
@@ -52,10 +56,9 @@ mod tests {
         let prefix: u128 = 1;
         // inside
         let ip_in: u128 = 1; // ::1 (loopback)
-        assert!(super::range_contains_ip(range_mask, prefix, ip_in));
+        assert!(super::range_contains_ip(prefix, range_mask, ip_in));
         // outside
         let ip_out: u128 = 1329227995784915872903807060280344577; // 100:0:0:0:0:0:0:1
-        assert_eq!(false, super::range_contains_ip(range_mask, prefix, ip_out));
+        assert_eq!(false, super::range_contains_ip(prefix, range_mask, ip_out));
     }
-
 }
