afc59c3ef96ce992abd6f0a653b2c0c6dd659006
jornfranke <oss@zuinnote.eu>
2026-08-16 18:41:20 +0200

    fix: larger buffer size to cover metadata

--- uprobe-libcall-filter/uprobe-libcall-filter-common/src/lib.rs
+++ uprobe-libcall-filter/uprobe-libcall-filter-common/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 // how much data can be exchanged in a buffer between eBPF and user space application in one go
-pub const DATA_BUF_CAPACITY: usize = 16384;
-pub const DATA_BUF_CAPACITY_CONTENT: usize = DATA_BUF_CAPACITY-16;
-// note: per SSL_read/SSL_write call OpenSSL supports up to 16 KB (cf. https://docs.openssl.org/3.0/man3/SSL_read/), ie it does not make sense to configure here much more
+pub const DATA_BUF_CAPACITY: usize = 32768;
+pub const DATA_BUF_CAPACITY_CONTENT: usize = DATA_BUF_CAPACITY - 16;
+// note: per SSL_read/SSL_write call OpenSSL supports up to 16 KB (cf. https://docs.openssl.org/3.0/man3/SSL_read/), ie it does not make sense to configure here much more (we do here 32K to cover also some metadata, such as size of data)
 // this is also according to the TLS specification: https://www.rfc-editor.org/rfc/rfc8446
